a21960e4a0425d51d6a2408804a653691cab6d02
Filipe Martins <66747698+filipoow@users.noreply.github.com>
2023-04-19 23:46:04 +0000

    Efeito no botão

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,15 @@
-var botao = document.getElementById("botao");
-
-botao.onclick = function() {
-  this.style.border = "2px solid red";
-};
+document.addEventListener("DOMContentLoaded", function() {
+    var botoes = document.querySelectorAll(".botao");
+  
+    for (var i = 0; i < botoes.length; i++) {
+      botoes[i].addEventListener("click", function() {
+        // remove a borda vermelha de todos os botões
+        for (var j = 0; j < botoes.length; j++) {
+          botoes[j].classList.remove("borda-clique");
+        }
+        // adiciona a borda vermelha apenas ao botão clicado
+        this.classList.add("borda-clique");
+      });
+    }
+  });
+  
